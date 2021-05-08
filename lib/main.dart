b97902fe6891a866d756ee6b7a3df74962d161f9
ironmask2004 <ironmask2004@gmail.com>
2021-05-08 14:45:22 +0300

    rtytry

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name  Main',
+      title: 'Startup Name  Main1111',
       home: RandomWords(),
     );
   }
